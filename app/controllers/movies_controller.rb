ebf087a97acad612d121edafaa95f2d54b690be0
Akwon89 <a.soonkwon@gmail.com>
2016-03-30 15:02:07 -0400

    pics added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -46,7 +46,7 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(
-      :title, :release_date, :director, :description, :runtime_in_minutes, :poster_image_url,
+      :title, :release_date, :director, :description, :runtime_in_minutes, :poster_image_url, :image
       )
   end
 
